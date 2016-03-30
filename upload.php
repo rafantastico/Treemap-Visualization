8c3282f0a3d041aa7003296773d512a54d3ed2cc
rodgom <rodgom@ugr.es>
2016-03-30 08:23:29 +0200

    Additional check to uploaded files

--- upload.php
+++ upload.php
@@ -1,56 +1,58 @@
 <?php
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $ERROR = 0;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-/*if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}*/
+
+//Allow only .mat file format
+if($imageFileType != "mat" ) {
+    $MESSAGE = "Sorry, only MAT files are allowed.";
+    $ERROR = 1;
+    $uploadOk = 0;
+    ?>
+    <script type='text/javascript'>
+        var mes = <?php echo json_encode($MESSAGE); ?>;
+        alert(mes);
+        window.location.replace("index.html");
+    </script>
+    <?php 
+    exit();
+}
+
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     $MESSAGE = "Sorry, your file is too large.";
     $ERROR = 1;
     $uploadOk = 0;
+    ?>
+    <script type='text/javascript'>
+        var mes = <?php echo json_encode($MESSAGE); ?>;
+        alert(mes);
+        window.location.replace("index.html");
+    </script>
+    <?php 
+    exit();
 }
-//Allow certain file formats
-if($imageFileType != "mat" ) {
-    $MESSAGE = "Sorry, only MAT files are allowed.";
-    $ERROR = 1;
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    $MESSAGE = "Sorry, your file was not uploaded.";
-    $ERROR = 1;
-// if everything is ok, try to upload file
-} else {
-    $value = $_POST["inputThr"];
-    if ((0 <= $value) && ($value <= 1)){
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 
-            $command = 'scripts/generateJavascript.py -t '.$_POST["inputThr"]. ' -i "uploads/'.basename($_FILES["fileToUpload"]["name"]).'"';
-            $MESSAGE = shell_exec($command);
-            if ($MESSAGE <> ""){
-                $ERROR = 1;
-            }
-        } else {
-            $MESSAGE = "Sorry, there was an error uploading your file.";
+//Upload the file
+$value = $_POST["inputThr"];
+if ((0 <= $value) && ($value <= 1)){
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+
+        $command = 'scripts/generateJavascript.py -t '.$_POST["inputThr"]. ' -i "uploads/'.basename($_FILES["fileToUpload"]["name"]).'"';
+        $MESSAGE = shell_exec($command);
+        if ($MESSAGE <> ""){
             $ERROR = 1;
         }
-    }else {
-        $MESSAGE = "Sorry, the entered threshold must be between 0 and 1.";
+    } else {
+        $MESSAGE = "Sorry, there was an error uploading your file.";
         $ERROR = 1;
     }
-    
+}else {
+    $MESSAGE = "Sorry, the entered threshold must be between 0 and 1.";
+    $ERROR = 1;
 }
 
 if($ERROR == 1){
@@ -71,7 +73,6 @@ else{
 	<link rel="stylesheet" type="text/css" href="visual.css">
 	<!-- Bootstrap core CSS -->
     <link href="bootstrap.min.css" rel="stylesheet">
-
     <!-- Custom styles for this template -->
     <link href="signin.css" rel="stylesheet">
 </head>
